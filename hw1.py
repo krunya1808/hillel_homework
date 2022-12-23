58a52ed0edb9aff9e87e01d57585c4869064e69f
krunya1808 <mashine211@gmail.com>
2022-12-23 11:52:00 -0500

    Programs id

---
+++ hw1.py
@@ -0,0 +1,6 @@
+your_str = input('Enter your string: ')
+str1 = your_str[0::2]
+str2 = your_str[::-1]
+print(your_str)
+print(str1)
+print(str2.upper())
---
+++ hw2.py
@@ -0,0 +1,15 @@
+a = 333
+b = 333
+c = 333
+print(id(a))
+print(id(b))
+print(id(c))
+
+a is b
+True
+
+a1 = 5
+a2 = 5
+a1 += 1
+print(id(a1))
+print(id(a2))
