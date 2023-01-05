ce5f3e7da379b2ce04cd531c69cd53fb20803b97
krunya1808 <mashine211@gmail.com>
2023-01-05 15:16:16 +0200

    hw_lambda

---
+++ hw8_func_count.py
@@ -0,0 +1,16 @@
+def count():
+    a = [1, 2, 3, 3, 5, 6, 2, 2, 5, 5, 6]
+    i = 0
+    val = 0
+    for i in a:
+        val += 1
+        print(a)
+
+rez = count()
+
+#list_numbers = [78, 99, 66, 44, 50, 30, 45, 15, 25, 20]
+#count = 0
+#for item in list_numbers:
+#    if item % 5 == 0:
+#        count += 1
+#print("количество элементов списка, удовлетворяющих заданному условию:", count)
